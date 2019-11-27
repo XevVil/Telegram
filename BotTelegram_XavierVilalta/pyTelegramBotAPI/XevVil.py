1ee90ae4a89336e4bdfc567d66df5c68bea0f49d
XevVil <56040472+XevVil@users.noreply.github.com>
2019-11-27 18:07:28 +0100

    Add files via upload

--- BotTelegram_XavierVilalta/pyTelegramBotAPI/XevVil.py
+++ BotTelegram_XavierVilalta/pyTelegramBotAPI/XevVil.py
@@ -5,36 +5,27 @@ import json
 
 bot = telebot.TeleBot("948463735:AAEpz18IkUQBGX3wTtaqsEk9G7IxjQJhlrY")
 
-
-json_lol_keyboard = json.dumps({'keyboard': [["o1", "o2"]],
-						   'one_time_keyboard': False,
-						   'resize_keyboard': True})
-
-json_build_keyboard = json.dumps({'keyboard': [["all", "items","skills"]],
-						   'one_time_keyboard': False,
-						   'resize_keyboard': True})
-
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(commands=['LeagueOfLegends','lol','help'])
+@bot.message_handler(commands=['LeagueOfLegends','lol'])
 def send_welcome(message):
 	bot.reply_to(message, "Welcome to the League of legends build helper.")
 
 @bot.message_handler(commands=['Alistar','alistar'])
-def send_welcome(message):
-	bot.send_message(message.chat.id,"Whitch position do you wanna play? (Recomended position: Support, Top.)",reply_markup=json_lol_keyboard)
+def character(message):
 
-
-
-
+	if message.text.split(' ')[1] == 'supp':
+		bot.send_message(message.chat.id,"Alistar Support", 'rb')
+		photo = open('Imatges/AlistarSuppRunes.png')
+		bot.send_photo(chat_id, photo)
+		#bot.send_message(message.chat.id,"Whitch position do you wanna play? (Recomended position: Support, Top.)",reply_markup = json_lol_keyboard)
 
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, 'aquest personatge no estar en memòria')
 
 bot.polling()
