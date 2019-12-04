a45ce760c0478e0b0b44204bfb5ce2856a4499aa
XevVil <56040472+XevVil@users.noreply.github.com>
2019-12-04 18:05:05 +0100

    Add files via upload

--- BotTelegram_XavierVilalta/pyTelegramBotAPI/XevVil.py
+++ BotTelegram_XavierVilalta/pyTelegramBotAPI/XevVil.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -"- coding: utf-8 -"-
 import telebot
-import json
 
 bot = telebot.TeleBot("948463735:AAEpz18IkUQBGX3wTtaqsEk9G7IxjQJhlrY")
 
@@ -17,10 +16,45 @@ def send_welcome(message):
 def character(message):
 
 	if message.text.split(' ')[1] == 'supp':
-		bot.send_message(message.chat.id,"Alistar Support", 'rb')
-		photo = open('Imatges/AlistarSuppRunes.png')
-		bot.send_photo(chat_id, photo)
-		#bot.send_message(message.chat.id,"Whitch position do you wanna play? (Recomended position: Support, Top.)",reply_markup = json_lol_keyboard)
+		bot.send_message(message.chat.id,"Alistar Support -> Runes, Skills , Items(essential items only)", 'rb')
+		photo = open('Imatges/Alistar/AlistarSuppRunes.png', )
+		bot.send_photo(message.chat.id,photo)
+		photo = open('Imatges/Alistar/AlistarSuppSkill.png', )
+		bot.send_photo(message.chat.id,photo)
+		photo = open('Imatges/Alistar/AlistarSuppItem.png', )
+		bot.send_photo(message.chat.id,photo)
+	else:
+		bot.send_message(message.chat.id,"Alistar is only support lmao, good luck playing this little shit somewhere else.", 'rb')
+
+
+@bot.message_handler(commands=['Aatrox','aatrox'])
+def character(message):
+	if message.text.split(' ')[1] == 'top':
+		bot.send_message(message.chat.id,"Aatrox Top -> Runes, Skills , Items(essential items only)", 'rb')
+		photo = open('Imatges/Aatrox/AatroxTopRunes.png', )
+		bot.send_photo(message.chat.id,photo)
+		photo = open('Imatges/Aatrox/AatroxTopSkill.png', )
+		bot.send_photo(message.chat.id,photo)
+		photo = open('Imatges/Aatrox/AatroxTopItem.png', )
+		bot.send_photo(message.chat.id,photo)
+
+	elif message.text.split(' ')[1] == 'jungle':
+		bot.send_message(message.chat.id,"Aatrox Jungler -> Runes, Skills , Items(essential items only)", 'rb')
+		photo = open('Imatges/Aatrox/AatroxJungleRunes.png', )
+		bot.send_photo(message.chat.id,photo)
+		photo = open('Imatges/Aatrox/AatroxTopSkill.png', )
+		bot.send_photo(message.chat.id,photo)
+		photo = open('Imatges/Aatrox/AatroxJungleItem.png', )
+		bot.send_photo(message.chat.id,photo)
+
+	elif message.text.split(' ')[1] == 'mid':
+		bot.send_message(message.chat.id,"Aatrox Jungler -> Runes, Skills , Items(essential items only)", 'rb')
+		photo = open('Imatges/Aatrox/AatroxMidRunes.png', )
+		bot.send_photo(message.chat.id,photo)
+
+	else:
+		bot.send_message(message.chat.id,"Alistar is only support lmao, good luck playing this little shit somewhere else.", 'rb')
+
 
 
 
