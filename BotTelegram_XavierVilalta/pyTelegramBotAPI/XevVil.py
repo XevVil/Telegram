20428ccdee0109df3ad51df2c09e2b95e429a5e5
XevVil <56040472+XevVil@users.noreply.github.com>
2019-12-18 14:24:36 +0100

    Add files via upload

--- BotTelegram_XavierVilalta/pyTelegramBotAPI/XevVil.py
+++ BotTelegram_XavierVilalta/pyTelegramBotAPI/XevVil.py
@@ -6,7 +6,9 @@ bot = telebot.TeleBot("948463735:AAEpz18IkUQBGX3wTtaqsEk9G7IxjQJhlrY")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Wellcome to LeagueOfIron, here you have a list of the champs you have to play")
+	bot.send_message(message.chat.id,"/alistar , /aatrox", '/kaisa', '/lucian', '/caitlyn' , '/thresh' 'rb')
+
 
 @bot.message_handler(commands=['LeagueOfLegends','lol'])
 def send_welcome(message):
@@ -14,22 +16,23 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['Alistar','alistar'])
 def character(message):
-
-	if message.text.split(' ')[1] == 'supp':
-		bot.send_message(message.chat.id,"Alistar Support -> Runes, Skills , Items(essential items only)", 'rb')
+		bot.send_message(message.chat.id,"Alistar Support (Only)-> Runes, Skills , Items(essential items only)", 'rb')
 		photo = open('Imatges/Alistar/AlistarSuppRunes.png', )
 		bot.send_photo(message.chat.id,photo)
 		photo = open('Imatges/Alistar/AlistarSuppSkill.png', )
 		bot.send_photo(message.chat.id,photo)
 		photo = open('Imatges/Alistar/AlistarSuppItem.png', )
 		bot.send_photo(message.chat.id,photo)
-	else:
-		bot.send_message(message.chat.id,"Alistar is only support lmao, good luck playing this little shit somewhere else.", 'rb')
+		bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
 
 @bot.message_handler(commands=['Aatrox','aatrox'])
 def character(message):
-	if message.text.split(' ')[1] == 'top':
+	bot.send_message(message.chat.id,"/aatrox_top, /aatrox_jung , /aatrox_mid", 'rb')
+
+@bot.message_handler(commands=['aatrox_top'])
+def character(message):
+
 		bot.send_message(message.chat.id,"Aatrox Top -> Runes, Skills , Items(essential items only)", 'rb')
 		photo = open('Imatges/Aatrox/AatroxTopRunes.png', )
 		bot.send_photo(message.chat.id,photo)
@@ -37,29 +40,88 @@ def character(message):
 		bot.send_photo(message.chat.id,photo)
 		photo = open('Imatges/Aatrox/AatroxTopItem.png', )
 		bot.send_photo(message.chat.id,photo)
+		bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
-	elif message.text.split(' ')[1] == 'jungle':
-		bot.send_message(message.chat.id,"Aatrox Jungler -> Runes, Skills , Items(essential items only)", 'rb')
-		photo = open('Imatges/Aatrox/AatroxJungleRunes.png', )
-		bot.send_photo(message.chat.id,photo)
-		photo = open('Imatges/Aatrox/AatroxTopSkill.png', )
-		bot.send_photo(message.chat.id,photo)
-		photo = open('Imatges/Aatrox/AatroxJungleItem.png', )
-		bot.send_photo(message.chat.id,photo)
+@bot.message_handler(commands=['aatrox_jung'])
+def character(message):
+	bot.send_message(message.chat.id,"Aatrox Jungler -> Runes, Skills , Items(essential items only)", 'rb')
+	photo = open('Imatges/Aatrox/AatroxJungleRunes.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Aatrox/AatroxTopSkill.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Aatrox/AatroxJungleItem.png', )
+	bot.send_photo(message.chat.id,photo)
+	bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
-	elif message.text.split(' ')[1] == 'mid':
+@bot.message_handler(commands=['aatrox_mid'])
+def character(message):
 		bot.send_message(message.chat.id,"Aatrox Jungler -> Runes, Skills , Items(essential items only)", 'rb')
 		photo = open('Imatges/Aatrox/AatroxMidRunes.png', )
 		bot.send_photo(message.chat.id,photo)
+		bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
-	else:
-		bot.send_message(message.chat.id,"Alistar is only support lmao, good luck playing this little shit somewhere else.", 'rb')
+@bot.message_handler(commands=['kaisa'])
+def character(message):
+	bot.send_message(message.chat.id,"Kai'sa ADC (Only)-> Runes, Skills , Items(essential items only)", 'rb')
+	photo = open('Imatges/Kaisa/KaisaAdcRunes.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Kaisa/KaisaAdcSkill.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Kaisa/KaisaAdcItem.png', )
+	bot.send_photo(message.chat.id,photo)
+	bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
+
+@bot.message_handler(commands=['lucian'])
+def character(message):
+	bot.send_message(message.chat.id,"/lucian_top, /lucian_mid, /lucian_adc", 'rb')
+
+@bot.message_handler(commands=['lucian_adc'])
+def character(message):
+	bot.send_message(message.chat.id,"Lucian ADC -> Runes, Skills , Items(essential items only)", 'rb')
+	photo = open('Imatges/Lucian/LucianAdcRunes.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Lucian/LucianAdcSkill.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Luacian/LucianAdcItem.png', )
+	bot.send_photo(message.chat.id,photo)
+	bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
 
+@bot.message_handler(commands=['lucian_top'])
+def character(message):
+	bot.send_message(message.chat.id,"Lucian Top -> Runes, Skills , Items(essential items only)", 'rb')
+	photo = open('Imatges/Lucian/LucianAdcRunes.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Lucian/LucianAdcSkill.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Luacian/LucianTopItem.png', )
+	bot.send_photo(message.chat.id,photo)
+	bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
+@bot.message_handler(commands=['lucian_mid'])
+def character(message):
+	bot.send_message(message.chat.id,"Lucian Mid -> Runes, Skills , Items(essential items only)", 'rb')
+	photo = open('Imatges/Lucian/LucianMidRunes.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Lucian/LucianAdcSkill.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Luacian/LucianTopItem.png', )
+	bot.send_photo(message.chat.id,photo)
+	bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
+
+@bot.message_handler(commands=['caitlyn'])
+def character(message):
+	bot.send_message(message.chat.id,"Caitlyn ADC (Only)-> Runes, Skills , Items(essential items only)", 'rb')
+	photo = open('Imatges/Caitlyn/CaitlynAdcRunes.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Caitlyn/CaitlynAdcSkill.png', )
+	bot.send_photo(message.chat.id,photo)
+	photo = open('Imatges/Caitlyn/CaitlynAdcItem.png', )
+	bot.send_photo(message.chat.id,photo)
+	bot.send_message(message.chat.id,"if you want to see another character click /start or /help.", 'rb')
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, 'aquest personatge no estar en memòria')
+	bot.reply_to(message, 'aquest personatge no està en memòria')
 
 bot.polling()
